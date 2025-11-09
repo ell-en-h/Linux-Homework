90f6514c3f94cd93e5fa749bc10a31f172311d18
ell-en-h <elen.hayrapetyan04@gmail.com>
2025-11-09 22:18:00 +0000

    Delete ENV-2/complex_sort/sort directory

--- ENV-2/complex_sort/sort/Sort.cpp
+++
@@ -1,8 +0,0 @@
-#include "Sort.h"
-#include <algorithm>
-
-void sortComplex(std::vector<Complex>& arr) {
-    std::sort(arr.begin(), arr.end(), [](const Complex& a, const Complex& b) {
-        return a.abs() < b.abs();
-    });
-}
