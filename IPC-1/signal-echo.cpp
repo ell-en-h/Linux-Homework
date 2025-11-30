83ea3704985543fde235338e1c1e323fe8bd2243
ell-en-h <elen.hayrapetyan04@gmail.com>
2025-11-30 18:32:39 +0000

    Update signal-echo.cpp

--- IPC-1/signal-echo.cpp
+++ IPC-1/signal-echo.cpp
@@ -3,6 +3,7 @@
 #include <signal.h>
 #include <pwd.h>
 #include <ucontext.h>
+#include <cstdio>
 
 void handler(int sig, siginfo_t* info, void* ucontext) {
     ucontext_t* ctx = (ucontext_t*)ucontext;
@@ -17,23 +18,28 @@ void handler(int sig, siginfo_t* info, void* ucontext) {
     unsigned long EAX = ctx->uc_mcontext.gregs[REG_RAX];
     unsigned long EBX = ctx->uc_mcontext.gregs[REG_RBX];
 
-    std::cout << "Received SIGUSR1 from PID " << sender_pid
-              << ", UID " << sender_uid
-              << " (" << username << ")\n";
+    char buf[256];
+    int n = snprintf(buf, sizeof(buf),
+        "Received SIGUSR1 from PID %d, UID %d (%s)\nRegisters: EIP=0x%lx EAX=0x%lx EBX=0x%lx\n",
+        sender_pid, sender_uid, username, EIP, EAX, EBX);
 
-    std::cout << "Registers: EIP=" << std::hex << EIP
-              << " EAX=" << EAX
-              << " EBX=" << EBX << std::dec << "\n";
+    write(STDOUT_FILENO, buf, n);
 }
 
 int main() {
-    std::cout << "Process PID = " << getpid() << "\n";
+    char buf[64];
+    int n = snprintf(buf, sizeof(buf), "Process PID = %d\n", getpid());
+    write(STDOUT_FILENO, buf, n);
 
     struct sigaction sa{};
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = handler;
-    sigaction(SIGUSR1, &sa, nullptr);
-
+    
+    if(sigaction(SIGUSR1, &sa, nullptr) == -1) {
+        const char* err = "sigaction failed\n";
+        write(STDOUT_FILENO, err, 17);
+        return 1;
+    }
     while (true) {
         sleep(10);
     }
